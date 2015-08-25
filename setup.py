e3c3831731e2843e254ee9674233dea058f0a51f
张少志 <david_bj@126.com>
2015-08-25 12:20:34 +0800

    update version 0.0.2

--- setup.py
+++ setup.py
@@ -1,10 +1,61 @@
 #!/usr/bin/env python
-
-import setuptools
+#import setuptools
+#
+#try:
+#    import multiprocessing
+#except ImportError:
+#    pass
+#
+#setuptools.setup(setup_requires=['pbr'], pbr=True)
+import codecs
+import os
+import sys
 
 try:
-    import multiprocessing
-except ImportError:
-    pass
+    from setuptools import setup
+except:
+    from distutils.core import setup
 
-setuptools.setup(setup_requires=['pbr'], pbr=True)
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+NAME = "pyzbx"
+
+PACKAGES = ["pyzbx",]
+
+DESCRIPTION = "python get zabbix data api interface."
+
+LONG_DESCRIPTION = read("README.md")
+
+KEYWORDS = "get zabbix data api."
+
+AUTHOR = "shaozhi.zhang"
+
+AUTHOR_EMAIL = "davidbjhd@gmail.com"
+
+URL = "https://github.com/davidbj/pyzbx"
+
+VERSION = "0.0.1"
+
+LICENSE = "MIT"
+
+setup(
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    keywords = KEYWORDS,
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = URL,
+    license = LICENSE,
+    packages = PACKAGES,
+    include_package_data = True,
+    zip_safe = True,
+)
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,31 @@
 #!/usr/bin/env python
-from pyzbx import ZabbixAPI
+#import sys
+#import os
+#pyzbx = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#sys.path.append(pyzbx)
+import pyzbx
+import unittest
+import httpretty
+import json
 
-#def test_getAuthcode():
+class TestPyzbx(unittest.TestCase):
+    '''pyzbx test class. '''
 
+    @httpretty.activate
+    def test_getAuthCode(self):
+        httpretty.register_uri(
+                httpretty.POST,
+                "http://zabbix.chunbo.com/zabbix/api_jsonrpc.php",
+                body=json.dumps({
+                    "jsonrpc": "2.0",
+                    "result": "693df78d1bad9a7b335b640846c56ccf",
+                    'id': 0
+                }),
+        )
+        zapi = pyzbx.ZbxAPI(zabbix_url="http://zabbix.chunbo.com/zabbix/api_jsonrpc.php", user="admin", password="Chunb0@2o14")
+
+        #check response
+        self.assertEqual(zapi.getAuthCode(), "693df78d1bad9a7b335b640846c56ccf")
+
+if __name__ == "__main__":
+    unittest.main()
